6e682cd580783a20d46d23daa00543caebf70ded
Jonathan Alonso <jonathanalonsom@outlook.com>
2023-05-22 09:28:11 -0300

    Chess layer and printing the board

--- src/application/program.java
+++ src/application/program.java
@@ -1,13 +1,15 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	
 	}
 
